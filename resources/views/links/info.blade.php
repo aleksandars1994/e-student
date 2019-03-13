e903a8a123697bc467c9c5beb372fde1597d181c
aleksandars1994 <aronijasajt@gmail.com>
2019-03-13 05:06:26 -0700

    Rad 13.Mart 2019

---
+++ resources/views/links/info.blade.php
@@ -0,0 +1,19 @@
+@extends('home')
+
+@section('info')
+	<div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Obavestenja</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Trenutno nema obavestenja
+                </div>
+            </div>
+        </div>
+@endsection
